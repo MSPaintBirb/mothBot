e5ea1a6182c8553c9699fa53b2f15e110dfc7d13
unknown <enrique.saldana01@utrgv.edu>
2022-10-13 19:23:07 -0500

    Initial Setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require("discord.js")
+require("dotenv").config()
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi"){
+        message.reply("Hello human")
+    }
+})
+
+client.login(process.env.TOKEN)
