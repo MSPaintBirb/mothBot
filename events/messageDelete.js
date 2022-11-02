a77d827dc166c22af732b593a093efb257184dc5
unknown <enrique.saldana01@utrgv.edu>
2022-11-01 23:08:11 -0500

    Fixed attachment bug

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -4,11 +4,20 @@ module.exports = {
         const channel = message.channel
         const user = message.author
 
-        // Runs into an error when the message was just an image,
-        // hence had empty text content
-        // Fix later
+        // Remember to change from author of message to me specifically
 
-        channel.send(message.content)
-        user.send(message.content)
+        user.send("Deleted message sent by: " + user)
+
+        if (message.content) {
+            user.send(message.content);
+        }
+        if (message.attachments) {
+            message.attachments.each(attachment => {
+                user.send({files: [attachment]});
+            })
+        }
+        else {
+            user.send("Was not able to get message content")
+        }
     }
 }
--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,12 @@
+const { ActivityType } = require("discord.js")
+
 module.exports = {
     name: "ready",
     run: async (bot) => {
         console.log("Logged in as " + bot.client.user.tag)
+
+        const { client } = bot
+
+        client.user.setActivity('L A M P', { type: ActivityType.Watching })
     }
 }
